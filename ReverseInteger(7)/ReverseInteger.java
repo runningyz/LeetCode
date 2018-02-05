d579b04a035df150709fd41b2628b9739d4eb328
runningyz <runningyangzhi@gmail.com>
2018-02-05 10:25:35 +0800

    fix ReverseInteger

--- ReverseInteger(7)/ReverseInteger.java
+++ ReverseInteger(7)/ReverseInteger.java
@@ -1,7 +1,11 @@
-import java.util.List;
-
 public class ReverseInteger{
 	public static void main(String args[]){
-		System.out.println("hello");
+		System.out.println(reverse(150));
+	}
+}
+
+class Solution{
+	public int reverse(int x){
+
 	}
 }
